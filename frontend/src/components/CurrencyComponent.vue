46cb597a5f9a6fc63f61b96b00054506d4b57c39
KetrabKox <coox144@gmail.com>
2024-03-09 12:40:26 +0100

    Added stock table

--- frontend/src/components/CurrencyComponent.vue
+++ frontend/src/components/CurrencyComponent.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class="wrapper rounded-4 d-flex flex-column align-items-center z-1">
-    <CurrencyElement />
+  <div
+    class="wrapper rounded-4 d-flex flex-column align-items-center z-1 overflow-scroll"
+  >
+    <CurrencyElement
+      v-for="(currency, index) in currencyName"
+      :key="index"
+      :currencyName="currency"
+      :currencyStock="currencyStock[index]"
+      :currencyValue="currencyValue[index]"
+    />
   </div>
 </template>
 <style scoped>
@@ -9,17 +17,73 @@
   width: 15vw;
   margin: 3rem 5% 0 auto;
   background-color: var(--secondary-theme-color);
-  padding: 0.5rem;
+  padding: 0.5rem 0 1rem 0;
 }
 </style>
 
 <script lang="ts">
 import CurrencyElement from "./CurrencyElement.vue";
 import { defineComponent } from "vue";
+import axios from "axios";
 
 export default defineComponent({
   components: {
     CurrencyElement,
+  },
+  data() {
+    return {
+      currencyValue: [],
+      currencyStock: [],
+      currencyName: [],
+    };
+  },
+  async mounted() {
+    var yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+    var today = new Date();
+
+    // Zmiana daty z weekendów na dni powszednie
+    if (today.getDay() == 6) {
+      today.setDate(today.getDate() - 1);
+      yesterday.setDate(yesterday.getDate() - 1);
+    } else if (today.getDay() == 0) {
+      today.setDate(today.getDate() - 2);
+      yesterday.setDate(yesterday.getDate() - 2);
+    }
+
+    // Formatowanie daty
+    var dd2 = String(yesterday.getDate()).padStart(2, "0");
+    var mm2 = String(yesterday.getMonth() + 1).padStart(2, "0");
+    var yyyy2 = yesterday.getFullYear();
+
+    var dd = String(today.getDate()).padStart(2, "0");
+    var mm = String(today.getMonth() + 1).padStart(2, "0");
+    var yyyy = today.getFullYear();
+
+    // Pobieranie danych z API NBP
+    let response_today = await axios.get(
+      `http://api.nbp.pl/api/exchangerates/tables/A/${
+        yyyy + "-" + mm + "-" + dd
+      }/`
+    );
+    let response_yesterday = await axios.get(
+      `http://api.nbp.pl/api/exchangerates/tables/A/${
+        yyyy2 + "-" + mm2 + "-" + dd2
+      }/`
+    );
+    // // Obliczanie różnicy dla kazdej waluty
+    for (let i = 0; i < response_today.data[0].rates.length; i++) {
+      let currency = response_today.data[0].rates[i].code;
+      let value_today = response_today.data[0].rates[i].mid.toFixed(3);
+      let value_yesterday = response_yesterday.data[0].rates[i].mid;
+      let stock = ((value_today - value_yesterday) / value_today) * 100;
+      if (value_today == 0) {
+        stock = 0;
+      }
+      this.currencyStock.push(stock as never);
+      this.currencyValue.push(value_today as never);
+      this.currencyName.push(currency as never);
+    }
   },
 });
 </script>
--- frontend/src/components/CurrencyElement.vue
+++ frontend/src/components/CurrencyElement.vue
@@ -1,8 +1,30 @@
 <template>
-  <button class="wrap rounded-4 mt-3">button</button>
+  <button class="wrap rounded-4 mt-3 d-flex flex-row p-0">
+    <div class="h-100 rounded-start-4">
+      <div class="currency value h-50 w-100 ps-2">{{ currencyValue }}</div>
+      <div class="currency name h-50 w-100 ps-2">{{ currencyName }}</div>
+    </div>
+    <div
+      class="currency stock h-100 w-100 rounded-end-4 stock-down"
+      v-if="currencyStock < 0"
+    >
+      {{ absoluteStock }}
+    </div>
+    <div
+      class="currency stock h-100 w-100 rounded-end-4 stock-up"
+      v-else-if="currencyStock >= 0"
+    >
+      {{ currencyStock.toFixed(3) }}
+    </div>
+  </button>
 </template>
 
 <style scoped>
+.currency {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 button {
   height: 8vh;
   width: 13vw;
@@ -18,4 +40,49 @@ button:active {
   box-shadow: 0 0.3rem var(--main-theme-color);
   transform: translateY(4px);
 }
+.currency.value {
+  border-top-left-radius: 15px;
+  font-size: 1.7rem;
+  font-weight: bold;
+}
+.currency.name {
+  border-bottom-left-radius: 15px;
+  font-size: 1.2rem;
+  font-weight: 100;
+}
+.currency.stock {
+  font-size: 1.7rem;
+  font-weight: 500;
+}
+.stock-down {
+  color: #bd0000;
+}
+.stock-up {
+  color: #25bc25;
+}
 </style>
+
+<script lang="ts">
+export default {
+  props: {
+    currencyValue: {
+      type: Number,
+      required: true,
+    },
+    currencyName: {
+      type: String,
+      required: true,
+    },
+    currencyStock: {
+      type: Number,
+      required: true,
+    },
+  },
+  // Obliczenie wartości bezwzględnej kursu waluty
+  computed: {
+    absoluteStock() {
+      return Math.abs(parseFloat(this.currencyStock.toFixed(3)));
+    },
+  },
+};
+</script>
